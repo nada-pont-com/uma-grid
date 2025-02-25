ccd5b7f37e52caae2b37c0ec9416d06ece9c8afd
Nada.pont.com <nada.pont.com@gmail.com>
2025-02-25 00:05:38 -0300

    Ajuste do Story Book.

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -8,6 +8,7 @@ const config: StorybookConfig = {
     '@chromatic-com/storybook',
     '@storybook/addon-interactions',
   ],
+  staticDirs: ['config'],
   framework: {
     name: '@storybook/react-vite',
     options: {},
